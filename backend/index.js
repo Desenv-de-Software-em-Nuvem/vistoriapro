90ea388f6dd12cd09cbaf65a53e5889fcf659c28
JSampx <joao.sampaio.andrade@gmail.com>
2026-03-12 14:09:54 -0300

    feat: documentação com Swagger (OpenAPI)

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 import express, { text } from "express";
 import pool from './src/config/database.js';
 import routes from './src/routes/index.js';
+// import dotenv from 'dotenv';
 
-const app = express();
+// dotenv.config();
+
+const app = express();  
 const PORT = 3000;
 
 app.get("/", (req, res) => {
