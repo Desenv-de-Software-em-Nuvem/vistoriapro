58e1f191a2681abab22462a3838fad61fc2d555f
DANIEL LEITE DELFINO <178340323+xdanielzdelfino@users.noreply.github.com>
2026-03-29 14:07:36 -0300

    Delete frontend/capacitor.config.ts

--- frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.vistoria.pro',
-  appName: 'vistoriapro',
-  webDir: 'dist'
-};
-
-export default config;
